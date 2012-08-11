155b80fe0cea6b9a0e2b1ec9cf2a51f311d6f45c
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- modules/iplog/language/english/admin.php
+++ modules/iplog/language/english/admin.php
@@ -31,41 +31,41 @@
  * 
  */
 
-define("_AM_IPLOG_SECONDS", "Seconds");
-define("_AM_IPLOG_UNKNOWN", "Unknown");
+define("_AM_IPLOG_SECONDS","Seconds");
+define("_AM_IPLOG_UNKNOWN","Unknown");
 
-define("_AM_IPLOG_TH_IP_ID", "IP ID");
-define("_AM_IPLOG_TH_UNAME", "Username");
-define("_AM_IPLOG_TH_IP", "IP");
-define("_AM_IPLOG_TH_COUNTRY_CODE", "Country Code");
-define("_AM_IPLOG_TH_NAME", "IP Country");
-define("_AM_IPLOG_TH_REGION", "IP Region");
-define("_AM_IPLOG_TH_CONTINENT", "IP Continent");
-define("_AM_IPLOG_TH_NETWORK_ADDY", "NetBIOS");
-define("_AM_IPLOG_TH_PROXY_IP", "Proxy IP");
-define("_AM_IPLOG_TH_AGENT", "User Agent");
-define("_AM_IPLOG_TH_START", "Start Session");
-define("_AM_IPLOG_TH_END", "End Session");
-define("_AM_IPLOG_TH_ONLINE", "Time Online");
-define("_AM_IPLOG_TH_ACTIONS", "Actions");
+define("_AM_IPLOG_TH_IP_ID","IP ID");
+define("_AM_IPLOG_TH_UNAME","Username");
+define("_AM_IPLOG_TH_IP","IP");
+define("_AM_IPLOG_TH_COUNTRY_CODE","Country Code");
+define("_AM_IPLOG_TH_NAME","IP Country");
+define("_AM_IPLOG_TH_REGION","IP Region");
+define("_AM_IPLOG_TH_CONTINENT","IP Continent");
+define("_AM_IPLOG_TH_NETWORK_ADDY","NetBIOS");
+define("_AM_IPLOG_TH_PROXY_IP","Proxy IP");
+define("_AM_IPLOG_TH_AGENT","User Agent");
+define("_AM_IPLOG_TH_START","Start Session");
+define("_AM_IPLOG_TH_END","End Session");
+define("_AM_IPLOG_TH_ONLINE","Time Online");
+define("_AM_IPLOG_TH_ACTIONS","Actions");
 
-define("_AM_IPLOG_LOG_H1", "Current User IP Log");
-define("_AM_IPLOG_LOG_P", "This is the current log of Users Logged in IP Addresses");
+define("_AM_IPLOG_LOG_H1","Current User IP Log");
+define("_AM_IPLOG_LOG_P","This is the current log of Users Logged in IP Addresses");
 
 //Dashboard
-define('_AM_IPLOG_ADMIN_COUNTS_BY_COUNTRY', 'Total Users Sessions from Countries');
-define('_AM_IPLOG_ADMIN_COUNTS_BY_REGION', 'Total Users Sessions from Region');
-define('_AM_IPLOG_ADMIN_COUNTS_BY_CONTINENT', 'Total Users Sessions from Continent');
-define('_AM_IPLOG_ADMIN_SUM_BY_COUNTRY', 'Number of minutes on per Country');
-define('_AM_IPLOG_ADMIN_SUM_BY_REGION', 'Number of minutes on per Region');
-define('_AM_IPLOG_ADMIN_SUM_BY_CONTINENT', 'Number of minutes on per Continent');
-define('_AM_IPLOG_ADMIN_AVG_BY_COUNTRY', 'Adverage Number of minutes on per Country');
-define('_AM_IPLOG_ADMIN_AVG_BY_REGION', 'Adverage Number of minutes on per Region');
-define('_AM_IPLOG_ADMIN_AVG_BY_CONTINENT', 'Adverage Number of minutes on per Continent');
+define('_AM_IPLOG_ADMIN_COUNTS_BY_COUNTRY','Total Users Sessions from Countries');
+define('_AM_IPLOG_ADMIN_COUNTS_BY_REGION','Total Users Sessions from Region');
+define('_AM_IPLOG_ADMIN_COUNTS_BY_CONTINENT','Total Users Sessions from Continent');
+define('_AM_IPLOG_ADMIN_SUM_BY_COUNTRY','Number of minutes on per Country');
+define('_AM_IPLOG_ADMIN_SUM_BY_REGION','Number of minutes on per Region');
+define('_AM_IPLOG_ADMIN_SUM_BY_CONTINENT','Number of minutes on per Continent');
+define('_AM_IPLOG_ADMIN_AVG_BY_COUNTRY','Adverage Number of minutes on per Country');
+define('_AM_IPLOG_ADMIN_AVG_BY_REGION','Adverage Number of minutes on per Region');
+define('_AM_IPLOG_ADMIN_AVG_BY_CONTINENT','Adverage Number of minutes on per Continent');
 
 //About
-define('_AM_IPLOG_ABOUT_MAKEDONATE', 'Make donation to Chronolabs co-op');
+define('_AM_IPLOG_ABOUT_MAKEDONATE','Make donation to Chronolabs co-op');
 
-define('_AM_MSG_LOG_DELETE', 'Are you sure to delete - %s (%s) - ID %s?');
-define('_AM_MSG_LOG_DELETED', 'IP Entry successfully deleted!');
+define('_AM_MSG_LOG_DELETE','Are you sure to delete - %s (%s) - ID %s?');
+define('_AM_MSG_LOG_DELETED','IP Entry successfully deleted!');
 ?>
--- modules/iplog/language/english/modinfo.php
+++ modules/iplog/language/english/modinfo.php
@@ -31,51 +31,51 @@
  * 
  */
 
-define("_MI_IPLOG_NAME", "User IP Logger");
-define("_MI_IPLOG_DESC", "Module for logging user IP Addresses");
+define("_MI_IPLOG_NAME","User IP Logger");
+define("_MI_IPLOG_DESC","Module for logging user IP Addresses");
 
 //Admin links
-define("_MI_IPLOG_INDEX", "Index");
-define("_MI_IPLOG_LOG", "IP Logs");
-define("_MI_IPLOG_GRAPHS", "Graphs");
-define('_MI_IPLOG_DASHBOARD', 'Dashboard');
-define('_MI_IPLOG_ABOUT', 'About Module');
+define("_MI_IPLOG_INDEX","Index");
+define("_MI_IPLOG_LOG","IP Logs");
+define("_MI_IPLOG_GRAPHS","Graphs");
+define('_MI_IPLOG_DASHBOARD','Dashboard');
+define('_MI_IPLOG_ABOUT','About Module');
 
 //Preferences
-define('_MI_IPLOG_LOGDROPS', 'Log Deletes Itself After');
-define('_MI_IPLOG_LOGDROPS_DESC', 'This is how long the log stays on your site for after a record reaches this age it is deleted!');
-define('_MI_IPLOG_LOGDROPS_24HOURS', '24 Hours');
-define('_MI_IPLOG_LOGDROPS_1WEEK', '1 Week');
-define('_MI_IPLOG_LOGDROPS_FORTNIGHT', 'A Fortnight');
-define('_MI_IPLOG_LOGDROPS_1MONTH', '1 Month');
-define('_MI_IPLOG_LOGDROPS_2MONTHS', '2 Months');
-define('_MI_IPLOG_LOGDROPS_3MONTHS', '3 Months');
-define('_MI_IPLOG_LOGDROPS_4MONTHS', '4 Months');
-define('_MI_IPLOG_LOGDROPS_5MONTHS', '5 Months');
-define('_MI_IPLOG_LOGDROPS_6MONTHS', '6 Months');
-define('_MI_IPLOG_LOGDROPS_12MONTHS', '1 Year');
-define('_MI_IPLOG_LOGDROPS_24MONTHS', '2 Years');
-define('_MI_IPLOG_LOGDROPS_36MONTHS', '3 Years');
+define('_MI_IPLOG_LOGDROPS','Log Deletes Itself After');
+define('_MI_IPLOG_LOGDROPS_DESC','This is how long the log stays on your site for after a record reaches this age it is deleted!');
+define('_MI_IPLOG_LOGDROPS_24HOURS','24 Hours');
+define('_MI_IPLOG_LOGDROPS_1WEEK','1 Week');
+define('_MI_IPLOG_LOGDROPS_FORTNIGHT','A Fortnight');
+define('_MI_IPLOG_LOGDROPS_1MONTH','1 Month');
+define('_MI_IPLOG_LOGDROPS_2MONTHS','2 Months');
+define('_MI_IPLOG_LOGDROPS_3MONTHS','3 Months');
+define('_MI_IPLOG_LOGDROPS_4MONTHS','4 Months');
+define('_MI_IPLOG_LOGDROPS_5MONTHS','5 Months');
+define('_MI_IPLOG_LOGDROPS_6MONTHS','6 Months');
+define('_MI_IPLOG_LOGDROPS_12MONTHS','1 Year');
+define('_MI_IPLOG_LOGDROPS_24MONTHS','2 Years');
+define('_MI_IPLOG_LOGDROPS_36MONTHS','3 Years');
 
-define('_MI_IPLOG_IPDB_APIKEY', 'IPDB API Key');
-define('_MI_IPLOG_IPDB_APIKEY_DESC', 'Register at <a href="http://ipinfodb.com/register.php">IPDB Registration</a> to recieve an API Key');
-define('_MI_IPLOG_USER_AGENT', 'cURL User-agent');
-define('_MI_IPLOG_USER_AGENT_DESC', 'This is the useragent used when cURL is instanciated');
-define('_MI_IPLOG_CURL_CONNECTION_TIMEOUT', 'cURL DNS Connection Timeout');
-define('_MI_IPLOG_CURL_CONNECTION_TIMEOUT_DESC', 'This is the number of seconds a DNS has to respond with cURL');
-define('_MI_IPLOG_CURL_TIMEOUT', 'cURL Communication Timeout');
-define('_MI_IPLOG_CURL_TIMEOUT_DESC', 'This is the number of seconds a server has to respond before cURL timesout');
-define('_MI_IPLOG_CURL_SSL_VERIFY_PEER', 'cURL Validates SSL Certificates');
-define('_MI_IPLOG_CURL_SSL_VERIFY_PEER_DESC', 'When this is enabled you must have a valid SSL Certificate when connecting with cURL to https://');
-define('_MI_IPLOG_CURL_VERBOSE', 'cURL is Verbose');
-define('_MI_IPLOG_CURL_VERBOSE_DESC', 'This is the option you can turn on to make cURL behave verbosely!');
+define('_MI_IPLOG_IPDB_APIKEY','IPDB API Key');
+define('_MI_IPLOG_IPDB_APIKEY_DESC','Register at <a href="http://ipinfodb.com/register.php">IPDB Registration</a> to recieve an API Key');
+define('_MI_IPLOG_USER_AGENT','cURL User-agent');
+define('_MI_IPLOG_USER_AGENT_DESC','This is the useragent used when cURL is instanciated');
+define('_MI_IPLOG_CURL_CONNECTION_TIMEOUT','cURL DNS Connection Timeout');
+define('_MI_IPLOG_CURL_CONNECTION_TIMEOUT_DESC','This is the number of seconds a DNS has to respond with cURL');
+define('_MI_IPLOG_CURL_TIMEOUT','cURL Communication Timeout');
+define('_MI_IPLOG_CURL_TIMEOUT_DESC','This is the number of seconds a server has to respond before cURL timesout');
+define('_MI_IPLOG_CURL_SSL_VERIFY_PEER','cURL Validates SSL Certificates');
+define('_MI_IPLOG_CURL_SSL_VERIFY_PEER_DESC','When this is enabled you must have a valid SSL Certificate when connecting with cURL to https://');
+define('_MI_IPLOG_CURL_VERBOSE','cURL is Verbose');
+define('_MI_IPLOG_CURL_VERBOSE_DESC','This is the option you can turn on to make cURL behave verbosely!');
 
 // Version 1.02
-define("_MI_IPLOG_GUEST", "Guest");
-define('_MI_IPLOG_ANONYMOUS', 'Allow Anonymous User to be Logged!');
-define('_MI_IPLOG_ANONYMOUS_DESC', 'Enabling this will mean that users that are not logged in are also logged!');
-define('_MI_IPLOG_IPV4', 'IPv4');
-define('_MI_IPLOG_IPV6', 'IPv6');
+define("_MI_IPLOG_GUEST","Guest");
+define('_MI_IPLOG_ANONYMOUS','Allow Anonymous User to be Logged!');
+define('_MI_IPLOG_ANONYMOUS_DESC','Enabling this will mean that users that are not logged in are also logged!');
+define('_MI_IPLOG_IPV4','IPv4');
+define('_MI_IPLOG_IPV6','IPv6');
 
 ?>
 
